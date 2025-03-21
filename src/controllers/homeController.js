b26c3767c53ccc5e6ea1e5e492cfdad913c74320
La Anh Quan <laanhquan1807@gmail.com>
2025-03-21 15:13:21 +0700

    video 32

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,5 +1,24 @@
+const connection = require('../config/database');
+
+
+
 const getHomepage = (req, res) => {
-    res.send('Hello World!')
+    //process data 
+    // call model 
+
+    let users = [];
+
+    connection.query(
+        'SELECT * FROM Users u',
+        function (err, results, fields) {
+            users = results;
+            console.log(">>> results = ", results); // results contains rows returned by server
+            console.log(">>> check users", users);
+            res.send(JSON.stringify(users))
+        }
+    );
+
+
 }
 
 const getABC = (req, res) => {
