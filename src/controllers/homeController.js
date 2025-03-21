e8389d4ab208ca3d5c4eabfcf161e3775015428e
La Anh Quan <laanhquan1807@gmail.com>
2025-03-21 23:58:39 +0700

    video 39

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,5 +1,5 @@
 const connection = require('../config/database');
-
+const mysql = require('mysql2/promise');
 
 
 const getHomepage = (req, res) => {
@@ -7,31 +7,30 @@ const getHomepage = (req, res) => {
     return res.render('home.ejs')
 }
 
-const postCreateUser = (req, res) => {
+const postCreateUser = async (req, res) => {
     console.log(">>> check req.body : ", req.body)
 
     let { email, myname, city } = req.body;
 
-    connection.query(
+    const [results, fields] = await connection.execute(
         `INSERT INTO 
         Users(email, name, city)
         VALUES (?, ?, ?)`,
         [email, myname, city],
-        function (err, results) {
-            console.log(">> check results ", results);
-            console.log(">> check err ", err);
-            // console.log(err)
-            return res.send('Create user success !')
-        }
+
     );
 
-    // INSERT INTO Users
-    // VALUES(3, 'test', 'eric va hoidanit', 'namdinh');
+    res.send('Create user success !')
+
 
 }
 
+
+const getCreatePage = (req, res) => {
+    return res.render('create.ejs')
+}
 const getABC = (req, res) => {
     res.render('sample.ejs')
 }
 
-module.exports = { getHomepage, getABC, postCreateUser }
+module.exports = { getHomepage, getABC, postCreateUser, getCreatePage }
